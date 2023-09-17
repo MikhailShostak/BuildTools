f82371221b80a05d0a7a8786ba3127c2c6bfc96f
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2023-09-17 13:44:24 +0200

    Remove extra output for Run.bat

--- Run.bat
+++ Run.bat
@@ -3,7 +3,7 @@ set ROOT=%~dp0
 set CONAN_HOME=%ROOT%\.conan
 set VS_VERSION=2022
 
-call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Professional\VC\Auxiliary\Build\vcvars64.bat"
-call %ROOT%\.venv\Scripts\activate.bat
+call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Professional\VC\Auxiliary\Build\vcvars64.bat" >NUL
+call %ROOT%\.venv\Scripts\activate.bat >NUL
 
 %*
