b54628462ae6b9aa0af9ccbe8ac581bdffb70691
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2023-11-19 23:20:19 +0100

    [DRAFT] Add different VS versions support

--- Run.bat
+++ Run.bat
@@ -3,7 +3,9 @@ set ROOT=%~dp0
 set CONAN_HOME=%ROOT%\.conan
 set VS_VERSION=2022
 
+call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Enterprise\VC\Auxiliary\Build\vcvars64.bat" >NUL
 call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Professional\VC\Auxiliary\Build\vcvars64.bat" >NUL
+call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL
 call %ROOT%\.venv\Scripts\activate.bat >NUL
 
 %*
