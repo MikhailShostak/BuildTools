626c12338f181965248ebdfccea6e26c89619189
Mikhail Shostak <MikhailShostak@users.noreply.github.com>
2023-12-10 05:35:43 +0100

    Fix Visual Studio selection

--- Run.bat
+++ Run.bat
@@ -3,13 +3,21 @@ setlocal
 set ROOT=%~dp0
 set CONAN_HOME=%ROOT%\.conan
 set VS_VERSION=2022
+set "VS_EDITIONS=Enterprise Professional Community"
 
+call %ROOT%\.venv\Scripts\activate.bat >NUL
 call %ROOT%\DevTools\conanrun
 
-REM call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Enterprise\VC\Auxiliary\Build\vcvars64.bat" >NUL
-REM call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Professional\VC\Auxiliary\Build\vcvars64.bat" >NUL
-REM call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\Community\VC\Auxiliary\Build\vcvars64.bat" >NUL
-call %ROOT%\.venv\Scripts\activate.bat >NUL
-
+where cmake >NUL 2>&1
+if %ERRORLEVEL% == 0 (
+    goto End
+)
+for %%i in (%VS_EDITIONS%) do (
+    if exist "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\%%i\VC\Auxiliary\Build\vcvars64.bat" (
+        call "%ProgramFiles%\Microsoft Visual Studio\%VS_VERSION%\%%i\VC\Auxiliary\Build\vcvars64.bat" >NUL
+        goto End
+    )
+)
+:End
 %*
 endlocal
